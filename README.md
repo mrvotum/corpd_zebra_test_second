4274ecf243284842b626d62e13839167db19660f
Vladimir <mr.qrsant@gmail.com>
2020-11-16 00:07:22 +0300

    Перезаписан репозиторий, добавлена интерактивная карта, новая вёрстка табов

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/97flt8fdb08dltjd/branch/main?svg=true)](https://ci.appveyor.com/project/mrvotum/corpd-zebra-test-second/branch/main)
+
+### link: https://mrvotum.github.io/corpd_zebra_test_second/
---
+++ src/js/widget.js
@@ -0,0 +1,63 @@
+export default class Widget {
+  constructor() {
+    this.widget = document.querySelector('[data-id=widget--tab]');
+    this.tabTitles = this.widget.querySelector('[data-id=tab__titles]');
+  }
+
+  create() {
+    this.addListener();
+    // this.createTabsWidth();
+  }
+
+  addListener() {
+    this.tabTitles.addEventListener('click', (event) => {
+      const clickedElement = event.target;
+      // Проверяем, что кликнули на вкладку
+      if (clickedElement.getAttribute('data-type') === 'tab') {
+        // Перекрасим элементы карты
+        this.kek(clickedElement.getAttribute('data-id'), this.widget.querySelector('[data-type=tab__content--active]').getAttribute('data-for'));
+        
+        // Скроем вкладку, что уже была открыта
+        this.hideOldTab();
+
+        // Проявим нужную вкладку
+        this.showNewTab(clickedElement);
+      }
+    });
+  }
+
+  showNewTab(clickedElement) {
+    const activeContent = this.widget.querySelector(`[data-for=${clickedElement.getAttribute('data-id')}]`);
+    activeContent.setAttribute('data-type', 'tab__content--active');
+    activeContent.classList.add('tab-el__content--active');
+
+    clickedElement.setAttribute('data-type', 'tab--active');
+    clickedElement.classList.add('tab-el__title--active');
+  }
+
+  hideOldTab() {
+    const activedContent = this.widget.querySelector('[data-type=tab__content--active]');
+    activedContent.setAttribute('data-type', 'tab__content');
+    activedContent.classList.remove('tab-el__content--active');
+
+    const activedTitle = this.widget.querySelector('[data-type=tab--active]');
+    activedTitle.setAttribute('data-type', 'tab');
+    activedTitle.classList.remove('tab-el__title--active');
+  }
+
+  createTabsWidth() {
+    for (let i = 0; i < this.tabTitles.childElementCount; i += 1) {
+      const element = this.tabTitles.children[i];
+      element.style.width = `calc(100% / ${this.tabTitles.childElementCount})`;
+    }
+  }
+
+  kek(oldTab, newTab) {
+    console.log(oldTab, newTab);
+    var svgDoc = document.getElementById("object-map");
+    var svg = svgDoc.contentDocument;
+    var obj = svg.getElementById("Layer_1");
+    obj.getElementById(newTab).style.fill = '#004062';
+    obj.getElementById(oldTab).style.fill = '#F39200';
+  }
+}
