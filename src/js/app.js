1ee28621d8155dc49b4d65a2511f5199577ce366
Vladimir <mr.qrsant@gmail.com>
2020-11-16 15:52:01 +0300

    Добавлено подобие API

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
 import Widget from './widget';
 
-const widget = new Widget();
+// data-id виджета, id объекта, содержащего svg, id самой svg
+const widget = new Widget('tab', 'object-map', 'Layer_1');
 widget.create();
+
+setTimeout(() => {
+  const clickedElement = document.querySelector('[data-id=tab_1]');
+  // Простенький API, если я всё правильно понял
+  widget.changeSvgColor('tab_5', 'tab_1', clickedElement);
+}, 500);
--- src/js/widget.js
+++ src/js/widget.js
@@ -1,7 +1,9 @@
 export default class Widget {
-  constructor() {
-    this.widget = document.querySelector('[data-id=tab]');
+  constructor(widgetId, objectId, svgId) {
+    this.widget = document.querySelector(`[data-id=${widgetId}]`);
     this.tabTitles = this.widget.querySelector('[data-id=tab__titles]');
+    this.svgDoc = document.getElementById(objectId);
+    this.obj = svgId;
   }
 
   create() {
@@ -9,29 +11,27 @@ export default class Widget {
   }
 
   addListener() {
-    this.tabTitles.addEventListener('click', (event) => {
-      const clickedElement = event.target;
-      // Проверяем, что кликнули на вкладку
-      if (clickedElement.getAttribute('data-type') === 'tab') {
+    const tabTitlesArr = this.widget.getElementsByClassName('tab-el__title');
+
+    tabTitlesArr.forEach((element) => {
+      element.addEventListener('click', (event) => {
+        this.clickedElement = event.target;
+
         // Перекрасим элементы карты
-        this.changeSvgColor(clickedElement.getAttribute('data-id'), this.widget.querySelector('[data-type=tab__content--active]').getAttribute('data-for'));
-
-        // Скроем вкладку, что уже была открыта
-        this.hideOldTab();
-
-        // Проявим нужную вкладку
-        this.showNewTab(clickedElement);
-      }
+        this.changeSvgColor(this.clickedElement.getAttribute('data-id'), this.widget.querySelector('[data-type=tab__content--active]').getAttribute('data-for'));
+      });
     });
   }
 
-  showNewTab(clickedElement) {
-    const activeContent = this.widget.querySelector(`[data-for=${clickedElement.getAttribute('data-id')}]`);
+  showNewTab() {
+    console.log(this.clickedElement);
+
+    const activeContent = this.widget.querySelector(`[data-for=${this.clickedElement.getAttribute('data-id')}]`);
     activeContent.setAttribute('data-type', 'tab__content--active');
     activeContent.classList.add('tab-el__content--active');
 
-    clickedElement.setAttribute('data-type', 'tab--active');
-    clickedElement.classList.add('tab-el__title--active');
+    this.clickedElement.setAttribute('data-type', 'tab--active');
+    this.clickedElement.classList.add('tab-el__title--active');
   }
 
   hideOldTab() {
@@ -42,14 +42,23 @@ export default class Widget {
     const activedTitle = this.widget.querySelector('[data-type=tab--active]');
     activedTitle.setAttribute('data-type', 'tab');
     activedTitle.classList.remove('tab-el__title--active');
+
+    // Проявим нужную вкладку
+    this.showNewTab();
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  changeSvgColor(oldTab, newTab) {
-    const svgDoc = document.getElementById('object-map');
-    const svg = svgDoc.contentDocument;
-    const obj = svg.getElementById('Layer_1');
+  changeSvgColor(oldTab, newTab, clickedElement) {
+    if (clickedElement) {
+      // Нужно для работы подобия API
+      this.clickedElement = clickedElement;
+    }
+
+    const svg = this.svgDoc.contentDocument;
+    const obj = svg.getElementById(this.obj);
     obj.getElementById(newTab).style.fill = '#004062';
     obj.getElementById(oldTab).style.fill = '#F39200';
+
+    // Скроем вкладку, что уже была открыта
+    this.hideOldTab();
   }
 }
